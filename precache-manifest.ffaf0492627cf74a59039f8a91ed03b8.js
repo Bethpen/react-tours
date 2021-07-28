466bc0505178f278616a95e40a56c5ce17df6119
Bethpen <64626802+Bethpen@users.noreply.github.com>
2021-07-28 18:45:18 +0100

    Updates

---
+++ precache-manifest.ffaf0492627cf74a59039f8a91ed03b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add84861a8a889c7b0e3c0e3003f5efa",
+    "url": "/react-tours/index.html"
+  },
+  {
+    "revision": "662d829e01482252c1ed",
+    "url": "/react-tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react-tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662d829e01482252c1ed",
+    "url": "/react-tours/static/js/main.3ab0358c.chunk.js"
+  },
+  {
+    "revision": "3361ebb3c243618fc7ca",
+    "url": "/react-tours/static/js/runtime-main.8e9505e0.js"
+  }
+]);
